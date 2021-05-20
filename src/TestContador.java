867437d89378dffee8d03e55bebcc519340e2fbb
CarlosPerezAlfonso <78152137+CarlosPerezAlfonso@users.noreply.github.com>
2021-05-20 11:52:53 +0100

    Test inicial e incremento por defecto FALLIDA

--- src/TestContador.java
+++ src/TestContador.java
@@ -14,6 +14,15 @@ public class TestContador {
 
         assertEquals(10, contador1.getValorLimite());
     }
+
+    @Test
+    //Test: inicial e incremento con valor 0 y 1 respectivamente por defecto
+    public void TestValoresPorDefecto() {
+        ClassContador contador1 = new ClassContador(10);
+
+        assertEquals(0, contador1.getValorInicial());
+        assertEquals(1, contador1.getValorIncremento());
+    }
     
 }
 
